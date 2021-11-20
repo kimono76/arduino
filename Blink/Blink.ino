1adca1bd2b8b96857d2734f631c09837aa8bd3ef
exo-fedora <kimono76.live@gmail.com>
2021-11-20 19:01:34 -0300

    feat:serialMonitor

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -2,12 +2,15 @@
 void setup() {
     //LED 13
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
     digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("HIGH");
     delay(500);
     
     digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LOW");
     delay(500);
 }
