b413da65db2552b7d01ab5b1203f219c145911ba
AlbertoGruning-lenvo-fedora <alberto.gruning.zen@gmail.com>
2021-12-22 21:48:23 -0300

    feat(serial): read write to serial and led #cap40

--- ReadSerial/ReadSerial.ino
+++ ReadSerial/ReadSerial.ino
@@ -1,17 +1,25 @@
 
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
     Serial.setTimeout(1000);
 }
 
-void loop() {
+void loop() { 
+    digitalWrite(LED_BUILTIN,LOW);
     String myString = "";
-    
+    int timer =200;
+
     while(myString.equals("")){
         myString = Serial.readString();
     }
-    if(!myString.equals("")){
+    if(!myString.equals("")){        
         Serial.println(myString);
+        if (myString[0] =='s')
+        {
+            timer= 5000;
+        } 
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(timer);
     }
-    //Serial.println(myString);
 }
